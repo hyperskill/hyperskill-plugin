61bd97e8532171ff3ec0f0382d8909c57533726e
meanmail <meanmail@meanmail.dev>
2025-07-18 23:18:28 +0400

    Replace `runBlockingCancellable` with `runBlocking` in `getInEdt` to simplify coroutine usage.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/openApiExt.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/openApiExt.kt
@@ -17,7 +17,6 @@ import com.intellij.openapi.fileTypes.impl.DetectedByContentFileType
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
-import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ex.ProjectEx
@@ -38,7 +37,7 @@ import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.TaskFile
 import com.jetbrains.edu.learning.courseFormat.isBinary
 import com.jetbrains.edu.learning.courseFormat.mimeFileType
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.annotations.TestOnly
 import org.jetbrains.annotations.VisibleForTesting
 import java.util.*
@@ -204,10 +203,8 @@ fun <T> withRegistryKeyOff(key: String, action: () -> T): T {
 }
 
 fun <V> getInEdt(modalityState: ModalityState = ModalityState.defaultModalityState(), compute: () -> V): V {
-  return runBlockingCancellable {
-    withContext(AppUIExecutor.onUiThread(modalityState).coroutineDispatchingContext()) {
-      compute()
-    }
+  return runBlocking(AppUIExecutor.onUiThread(modalityState).coroutineDispatchingContext()) {
+    compute()
   }
 }
 
