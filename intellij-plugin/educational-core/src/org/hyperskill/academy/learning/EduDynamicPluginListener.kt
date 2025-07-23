ddfd75cb08833be84737f94d89780981cdb40149
meanmail <meanmail@meanmail.dev>
2025-07-23 16:34:17 +0400

    Remove unused `checkUnloadPlugin` method in `EduDynamicPluginListener`.

--- intellij-plugin/educational-core/src/org/hyperskill/academy/learning/EduDynamicPluginListener.kt
+++ intellij-plugin/educational-core/src/org/hyperskill/academy/learning/EduDynamicPluginListener.kt
@@ -7,10 +7,6 @@ import com.intellij.openapi.extensions.PluginId
 
 class EduDynamicPluginListener : DynamicPluginListener, DynamicPluginVetoer {
 
-  override fun checkUnloadPlugin(pluginDescriptor: IdeaPluginDescriptor) {
-    // No need to throw an exception, the vetoPluginUnload method will be called
-  }
-
   override fun vetoPluginUnload(pluginDescriptor: IdeaPluginDescriptor): String? {
     if (pluginDescriptor.pluginId == PluginId.getId(EduNames.PLUGIN_ID)) {
       return "Hyperskill Academy plugin unloading is not supported yet"
