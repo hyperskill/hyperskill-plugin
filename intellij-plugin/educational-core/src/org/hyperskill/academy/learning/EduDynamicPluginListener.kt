c828694b4cd369873c53d098f1e9c97b52789487
meanmail <meanmail@meanmail.dev>
2025-07-23 16:24:43 +0400

    Refactor `EduDynamicPluginListener` to implement `DynamicPluginVetoer` and replace exception throwing with unload veto messaging.

--- intellij-plugin/educational-core/src/org/hyperskill/academy/learning/EduDynamicPluginListener.kt
+++ intellij-plugin/educational-core/src/org/hyperskill/academy/learning/EduDynamicPluginListener.kt
@@ -1,15 +1,20 @@
 package org.hyperskill.academy.learning
 
-import com.intellij.ide.plugins.CannotUnloadPluginException
 import com.intellij.ide.plugins.DynamicPluginListener
+import com.intellij.ide.plugins.DynamicPluginVetoer
 import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.openapi.extensions.PluginId
 
-class EduDynamicPluginListener : DynamicPluginListener {
+class EduDynamicPluginListener : DynamicPluginListener, DynamicPluginVetoer {
 
   override fun checkUnloadPlugin(pluginDescriptor: IdeaPluginDescriptor) {
+    // No need to throw an exception, the vetoPluginUnload method will be called
+  }
+
+  override fun vetoPluginUnload(pluginDescriptor: IdeaPluginDescriptor): String? {
     if (pluginDescriptor.pluginId == PluginId.getId(EduNames.PLUGIN_ID)) {
-      throw CannotUnloadPluginException("Hyperskill Academy plugin unloading is not supported yet")
+      return "Hyperskill Academy plugin unloading is not supported yet"
     }
+    return null
   }
 }
