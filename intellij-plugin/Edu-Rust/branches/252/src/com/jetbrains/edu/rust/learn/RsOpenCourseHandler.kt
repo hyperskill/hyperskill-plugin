487d01a7d0a832c9264d3fdcc472379f58fa50ff
meanmail <meanmail@meanmail.dev>
2025-07-24 14:31:54 +0400

    Fix

--- intellij-plugin/Edu-Rust/branches/252/src/com/jetbrains/edu/rust/learn/RsOpenCourseHandler.kt
+++ intellij-plugin/Edu-Rust/branches/252/src/com/jetbrains/edu/rust/learn/RsOpenCourseHandler.kt
@@ -6,7 +6,7 @@ import java.nio.file.Path
 
 class RsOpenCourseHandler : OpenCourseHandler {
   override suspend fun openCourse(courseId: Int, toolchain: RsToolchainBase, projectLocation: Path?) {
-    RsOpenCourseHelper.openCourse(courseId, toolchain, projectLocation)
+    RsOpenCourseHelper.openCourse(courseId)
   }
 
   override fun isAlreadyStartedCourse(courseId: Int): Boolean {
