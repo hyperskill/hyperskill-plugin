115746e2b625d1860d2e32958986cad600757fb0
meanmail <meanmail@meanmail.dev>
2025-10-29 15:57:28 +0400

    Update `hs-jvm-core` test task to prevent failure on zero discovered tests

--- intellij-plugin/hs-jvm-core/build.gradle.kts
+++ intellij-plugin/hs-jvm-core/build.gradle.kts
@@ -13,3 +13,9 @@ dependencies {
 
   testImplementation(project(":intellij-plugin:hs-core", "testOutput"))
 }
+
+// hs-jvm-core contains only abstract test bases/utilities; allow zero discovered tests
+tasks.test {
+  // Gradle 8+: do not fail the build when no tests are discovered
+  failOnNoDiscoveredTests = false
+}
