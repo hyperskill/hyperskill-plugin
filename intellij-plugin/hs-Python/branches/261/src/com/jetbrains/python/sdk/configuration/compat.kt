3bda0a2d016c8e2d558794dc451e662aa28fdd22
Andrew Kozlov <adkozlov@gmail.com>
2026-03-15 23:46:15 +0100

    ALT-10993 transferred to BGT

--- intellij-plugin/hs-Python/branches/261/src/com/jetbrains/python/sdk/configuration/compat.kt
+++ intellij-plugin/hs-Python/branches/261/src/com/jetbrains/python/sdk/configuration/compat.kt
@@ -110,7 +110,16 @@ fun createVirtualEnvAndSdkSynchronously(
     // The method `onVirtualEnvCreated(..)` stores preferred base path to virtual envs. Storing here the base path from the non-local
     // target (e.g. a path from SSH machine or a Docker image) ends up with a meaningless default for the local machine.
     // If we would like to store preferred paths for non-local targets we need to use some key to identify the exact target.
-    PySdkSettings.instance.onVirtualEnvCreated(installedSdk.homePath, FileUtil.toSystemIndependentName(venvRoot), projectPath)
+
+    object : Task.Backgroundable(project, "...") {
+      override fun run(progressIndicator: ProgressIndicator) {
+        PySdkSettings.instance.onVirtualEnvCreated(
+          sdkPath = installedSdk.homePath,
+          location = FileUtil.toSystemIndependentName(venvRoot),
+          projectPath = projectPath,
+        )
+      }
+    }
   }
 
   return venvSdk
