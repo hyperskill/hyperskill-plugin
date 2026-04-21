30f8139708abf1cbda6242cf4ca6255643067940
Oleksandr Liemiahov <tsyklop@gmail.com>
2026-04-21 18:03:52 +0300

    ALT-11006
    fix build

--- intellij-plugin/hs-core/branches/261/src/org/hyperskill/academy/platform/OpenProjectTaskCompat.kt
+++ intellij-plugin/hs-core/branches/261/src/org/hyperskill/academy/platform/OpenProjectTaskCompat.kt
@@ -3,6 +3,7 @@ package org.hyperskill.academy.platform
 import com.intellij.ide.impl.OpenProjectTask
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import java.nio.file.Path
 
 /**
  * Compatibility helper to construct OpenProjectTask for 253+.
@@ -20,15 +21,33 @@ object OpenProjectTaskCompat {
     beforeInit: ((Project) -> Unit)? = null,
     preparedToOpen: ((Project, Module) -> Unit)? = null
   ): OpenProjectTask {
-    return OpenProjectTask {
-      this.forceOpenInNewFrame = forceOpenInNewFrame
-      this.isNewProject = isNewProject
-      this.isProjectCreatedWithWizard = isProjectCreatedWithWizard
-      this.runConfigurators = runConfigurators
-      this.projectName = projectName
-      this.projectToClose = projectToClose
-      if (beforeInit != null) this.beforeInit = { beforeInit(it) }
-      if (preparedToOpen != null) this.preparedToOpen = { preparedToOpen(it.project, it) }
-    }
+    return OpenProjectTask(
+      forceOpenInNewFrame = forceOpenInNewFrame,
+      forceReuseFrame = false,
+      projectToClose = projectToClose,
+      isNewProject = isNewProject,
+      useDefaultProjectAsTemplate = true,
+      project = null,
+      projectName = projectName,
+      showWelcomeScreen = true,
+      callback = null,
+      line = -1,
+      column = -1,
+      isRefreshVfsNeeded = false,
+      runConfigurators = runConfigurators,
+      runConversionBeforeOpen = true,
+      projectWorkspaceId = null,
+      projectFrameTypeId = null,
+      isProjectCreatedWithWizard = isProjectCreatedWithWizard,
+      preloadServices = false,
+      beforeInit = if (beforeInit != null) { { beforeInit(it) } } else null,
+      beforeOpen = null,
+      preparedToOpen = if (preparedToOpen != null) { { preparedToOpen(it.project, it) } } else null,
+      preventIprLookup = false,
+      processorChooser = null,
+      implOptions = null,
+      projectRootDir = null,
+      createModule = true
+    )
   }
 }
