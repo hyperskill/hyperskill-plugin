9ba7676c241dfa53291baf4be90ba12c874d3a8c
Oleksandr Liemiahov <tsyklop@gmail.com>
2026-04-27 11:36:34 +0300

    ALT-11011 [IDE Plugin] Problems opening a task in the IDE

--- intellij-plugin/hs-core/branches/261/src/org/hyperskill/academy/platform/OpenProjectTaskCompat.kt
+++ intellij-plugin/hs-core/branches/261/src/org/hyperskill/academy/platform/OpenProjectTaskCompat.kt
@@ -3,7 +3,6 @@ package org.hyperskill.academy.platform
 import com.intellij.ide.impl.OpenProjectTask
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import java.nio.file.Path
 
 /**
  * Compatibility helper to construct OpenProjectTask for 253+.
@@ -21,21 +20,24 @@ object OpenProjectTaskCompat {
     beforeInit: ((Project) -> Unit)? = null,
     preparedToOpen: ((Project, Module) -> Unit)? = null
   ): OpenProjectTask {
-    return OpenProjectTask {
-      this.forceOpenInNewFrame = forceOpenInNewFrame
-      this.forceReuseFrame = false
-      this.isNewProject = isNewProject
-      this.isProjectCreatedWithWizard = isProjectCreatedWithWizard
-      this.runConfigurators = runConfigurators
-      this.projectName = projectName
-      this.projectToClose = projectToClose
-      this.createModule = true
-      this.useDefaultProjectAsTemplate = true
-      this.runConversionBeforeOpen = true
-      this.showWelcomeScreen = true
-      this.preloadServices = false
-      if (beforeInit != null) this.beforeInit = { beforeInit(it) }
-      if (preparedToOpen != null) this.preparedToOpen = { preparedToOpen(it.project, it) }
-    }
+
+    val task = OpenProjectTask(
+      forceOpenInNewFrame = forceOpenInNewFrame,
+      projectToClose = projectToClose,
+      isNewProject = isNewProject
+    )
+    return task.copy(
+      projectName = projectName,
+      runConfigurators = runConfigurators,
+      isProjectCreatedWithWizard = isProjectCreatedWithWizard,
+      beforeInit = beforeInit?.let { { beforeInit(it) } },
+      preparedToOpen = preparedToOpen?.let { { preparedToOpen(it.project, it) } },
+      forceReuseFrame = false,
+      createModule = true,
+      useDefaultProjectAsTemplate = true,
+      runConversionBeforeOpen = true,
+      showWelcomeScreen = true,
+      preloadServices = false
+    )
   }
 }
