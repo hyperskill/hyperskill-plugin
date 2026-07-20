db806e487fa2a94b512b620196cc13388fe27fcd
Oleksandr Liemiahov <tsyklop@gmail.com>
2026-07-20 11:48:23 +0300

    fix Compatibility

--- intellij-plugin/hs-Cpp/branches/262/src/org/hyperskill/academy/cpp/checker/CppCatchEduTaskChecker.kt
+++ intellij-plugin/hs-Cpp/branches/262/src/org/hyperskill/academy/cpp/checker/CppCatchEduTaskChecker.kt
@@ -2,7 +2,6 @@ package org.hyperskill.academy.cpp.checker
 
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.openapi.project.Project
-import com.jetbrains.cidr.execution.testing.tcatch.CidrCatchTestRunConfigurationType
 import org.hyperskill.academy.cpp.CppCatchCourseBuilder
 import org.hyperskill.academy.cpp.CppConfigurator
 import org.hyperskill.academy.cpp.CppProjectSettings
@@ -20,7 +19,7 @@ class CppCatchConfigurator : CppConfigurator() {
       override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> =
         object : CppEduTaskChecker(task, envChecker, project) {
           override fun getFactory(): ConfigurationFactory =
-            CidrCatchTestRunConfigurationType.getInstance().getFactory()
+            getTestConfigurationFactory(CATCH_TEST_CONFIGURATION_TYPE_ID)
         }
     }
 }
